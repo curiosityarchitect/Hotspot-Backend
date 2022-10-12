164e80b21b5d412f614e91c36c2be825c22dc283
fuzzyhappy <student.e.wang@gmail.com>
2022-10-12 12:46:10 -0400

    Completed user searching

--- src/api-routes/users.routes.ts
+++ src/api-routes/users.routes.ts
@@ -1,12 +1,53 @@
 import { Router, Request, Response, RequestHandler } from "express";
 import { User } from "../schema/user.schema"
+import * as mongoose from "mongoose";
 
 const userRouter: Router = Router();
 
 userRouter.route('/users').get((req: Request, res: Response) => {
-    User.find({})
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json("Error: No users found"));
+    if (req.query.search) {
+        let starts: object[] = [];
+        let contains: object[] = [];
+        const startRegex = `^(${req.query.search}).*`;
+        const containsRegex = `.*(${req.query.search}).*`;
+        User.find({
+            username: {
+                $regex : startRegex
+            }
+        })
+        .lean() // returns a json not a document
+        .then(users => {
+            starts = users;
+            // tslint:disable-next-line:no-console
+            console.log(starts);
+
+            // creates array of user IDs that have already been found
+            const alreadyFound: mongoose.Types.ObjectId[] = users.map(user => user._id);
+            return User.find({
+                username: {
+                    $regex : containsRegex
+                },
+                _id: {
+                    $nin : alreadyFound
+                }
+            }).lean();
+        })
+        .then(users => {
+            contains = users;
+            // tslint:disable-next-line:no-console
+            console.log(contains);
+
+            const result = starts.concat(contains);
+            // tslint:disable-next-line:no-console
+            console.log(result);
+            res.json(result);
+        })
+        .catch(err => res.status(400).json("Error: No users found"));
+    } else {
+        User.find({})
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json("Error: No users found"));
+    }
 });
 
 userRouter.route('/users').post((req: Request, res: Response) => {
@@ -20,6 +61,12 @@ userRouter.route('/users').post((req: Request, res: Response) => {
     newUser.save()
     .then(() => res.json('User added'))
     .catch(err => res.status(400).json("ERROR: user could not be added"));
+});
+
+userRouter.route('/users/:userId').get((req: Request, res: Response) => {
+    User.findById(req.params.userId)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json(err));
 });
 
 export default userRouter;
