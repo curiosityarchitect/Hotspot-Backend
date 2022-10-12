9ccdd5bf9e6d81c22f5afd1b9a0e41c557cd9bef
fuzzyhappy <student.e.wang@gmail.com>
2022-10-11 22:38:29 -0400

    Fixed tslint issues

--- src/api-routes/friend-request.routes.ts
+++ src/api-routes/friend-request.routes.ts
@@ -3,16 +3,16 @@ import { friendRequest } from "../schema/friend_request.schema"
 import { User } from "../schema/user.schema"
 
 
-const fr_reqRouter: Router = Router();
+const friendRequestRouter: Router = Router();
 
-fr_reqRouter.route('/friend-requests').post((req: Request, res: Response) => {
+friendRequestRouter.route('/friend-requests').post((req: Request, res: Response) => {
     const reciever = req.body.reciever;
     const deliver = req.body.deliver;
     const newRequest = new friendRequest ({
         reciever,
         deliver
     });
-    //How to make the if statement. If there will be no users found, I would not make the newRequest.save
+    // How to make the if statement. If there will be no users found, I would not make the newRequest.save
     User.find({username: reciever})
     .then(() => {
         newRequest.save();
@@ -21,11 +21,11 @@ fr_reqRouter.route('/friend-requests').post((req: Request, res: Response) => {
     .catch(err => res.status(400).json("Error: No users found"));
 });
 
-fr_reqRouter.route('/friend-requests').get((req: Request, res: Response) => {
+friendRequestRouter.route('/friend-requests').get((req: Request, res: Response) => {
     const username = req.body.username;
     friendRequest.find({reciever: username})
-    .then(fr_req => res.json(fr_req))
+    .then(friendReq => res.json(friendReq))
     .catch(err => res.status(400).json("Error: No requests found!"));
 });
 
-export default fr_reqRouter;
+export default friendRequestRouter;
