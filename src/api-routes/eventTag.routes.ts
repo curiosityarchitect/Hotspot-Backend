da6cccbb618519db2a98c257af0e2530c1083d43
Jony <66816587+Jonystorm1@users.noreply.github.com>
2022-10-22 22:45:14 -0400

    first draft of tagSchema, tag endpoints, tag quering, and tag-based event search
    made the first draft. Discussion needed

---
+++ src/api-routes/eventTag.routes.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response, RequestHandler } from "express";
+import { Tag } from "../schema/tags.schema"
+import * as mongoose from "mongoose";
+import { Events } from "../schema/events.schema"
+import { EventTag } from "../schema/eventTags.schema";
+
+const eventTagsRouter: Router = Router();
+
+eventTagsRouter.route('/tags').post((req: Request, res: Response) => {
+    const name = req.body.name;
+    const id = req.body.id;
+    Tag.find({
+        name: name
+    })
+    .then()
+    .catch(err => res.status(400).json("ERROR: tag could not be found"));
+    Events.findById(id)
+    .then()
+    .catch(err => res.status(400).json("ERROR: event could not be found"))
+    const newEventTag = new EventTag({
+        name,
+        id
+    });
+    newEventTag.save()
+    .then(() => res.json('Tag is added to the event'))
+    .catch(err => res.status(400).json("ERROR: tag could not be added"));
+});
