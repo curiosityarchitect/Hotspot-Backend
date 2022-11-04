0a7beffbee78cc848c7122bad95910c59bacc3ad
Alex Wu <wu1441@purdue.edu>
2022-11-03 23:54:03 -0400

    Completed Friend Request with extra features

--- src/api-routes/friend-request.routes.ts
+++ src/api-routes/friend-request.routes.ts
@@ -15,41 +15,71 @@ friendRequestRouter.route('/friend-requests').post((req: Request, res: Response)
     });
     // How to make the if statement. If there will be no users found, I would not make the newRequest.save
     User.find({username: reciever})
-    .then(() => {
-        newRequest.save();
-        res.json('Request has been sent!');
+    friends.findOne({username1: reciever, username2: deliverer}) // handle duplicate friend requests
+    .then((relation) => {
+        if (relation === null) {
+            newRequest.save();
+            res.json('Request has been sent!');
+        }
+        else {
+            res.json('You are already friends!');
+        }
     })
-    .catch(err => res.status(400).json("Error: No users found"));
+    .catch(err => res.status(err).json("Error: No users found"));
 });
 // handle yes/no for friend request
-friendRequestRouter.route('/friend-requests/status/:username').post((req: Request, res: Response) => {
+
+// accept
+friendRequestRouter.route('/friend-requests/status/:username/accept').post((req: Request, res: Response) => {
     const username = req.params.username;
+    const deliverer = req.body.deliverer;
     const friendRelation = new friends ({
         username1 : username,
-        username2: req.body.username
-    });
-    friendRequest.findOneAndDelete({username1: username, username2: req.body.username}) // if the request is accepted, delete the request
+        username2: deliverer
+    }); // if the request is accepted, delete the request and create friend relation
+     friendRequest.findOneAndDelete({reciever: username, deliverer})
     .then(() => {
-        friendRelation.save();
-        res.json('Request has been accepted!');
+        return friends.findOne({username1: username, username2: deliverer})
     })
-    .catch(err => res.status(400).json("Error: No requests found!"));
+    .then((newFriend) => {
+        if (newFriend === null) {
+            friendRelation.save();
+            res.json('Request has been accepted!');
+        } else {
+            res.json('You are already friends!');
+        }
+    })
+    .catch(err => res.status(err).json("Error: No requests found!"));
 });
+
+// decline
+friendRequestRouter.route('/friend-requests/status/:username/decline').post((req: Request, res: Response) => {
+    const username1 = req.params.username;
+    const username2 = req.body.username;
+    friendRequest.findOneAndDelete({reciever: username1, deliverer: username2}) // if the request is declined, delete the request and dont create friend relation
+    .then(() => {
+        res.json('Request has been rejected!');
+    })
+    .catch(err => res.status(err).json("Error: No requests found!"));
+});
+
+
+
 friendRequestRouter.route('/friend-requests/status/:username').get((req: Request, res: Response) => {
     const username = req.params.username;
-    friends.find({username1: username,username2: req.body.username})
+    friends.findOne({username1: username,username2: req.body.username})
     .then(
         friendRelation => res.json(friendRelation) // see if user2 is already friends with user1
     )
-    .catch(err => res.status(400).json("Error: No requests found!"));
+    .catch(err => res.status(err).json("Error: No relation found"));
 });
 
 
-friendRequestRouter.route('/friend-requests').get((req: Request, res: Response) => {
-    const username = req.body.username;
+friendRequestRouter.route('/friend-requests/:username').get((req: Request, res: Response) => {
+    const username = req.params.username;
     friendRequest.find({reciever: username})
     .then(friendReq => res.json(friendReq))
-    .catch(err => res.status(400).json("Error: No requests found!"));
+    .catch(err => res.status(err).json("Error: No requests found!"));
 });
 
 export default friendRequestRouter;
